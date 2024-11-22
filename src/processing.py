90c6175430113b252dbbb431567cf9a1bf2b40c2
Your Name <you@example.com>
2024-11-22 19:40:32 +0400

    processing.py: second func

--- src/processing.py
+++ src/processing.py
@@ -6,9 +6,8 @@ def filter_by_state(proced_list: list[dict[str, any]], state: str = 'EXECUTED') 
 print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
 
 def sort_by_date(proced_list: list[dict[str, any]], reverse_list: bool = True) -> list[dict[str, any]]:
-    for operation in proced_list:
-        for key, value in operation.items():
-            sorted_list = sorted(proced_list, key=lambda x: x['date'])
+    """Функция принимает список словарей с операциями и сортирует их по дате"""
+    sorted_list = sorted(proced_list, key=lambda x: x['date'], reverse = reverse_list)
     return sorted_list
 
 print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
