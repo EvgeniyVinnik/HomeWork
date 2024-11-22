a9de64fe18122bf2b0f066d9c6d97bcc69be6c84
Your Name <you@example.com>
2024-11-22 19:27:51 +0400

    processing.py: first func

---
+++ src/processing.py
@@ -0,0 +1,6 @@
+filter_param = {'state' : 'EXECUTED'}
+def filter_by_state(proced_list: list[dict[str, any]], state: str = 'EXECUTED') -> any:
+    """Функция принимает на вход список словарей с операциями и возвращает список с нужными параметрами"""
+    return [t for t in proced_list if t.get("state") == state]
+
+print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
